5c4b1f9fb6d8a326f72a8d091fb8b0580876b45a
1t-n00b <mummintrol@gmail.com>
2022-08-03 15:09:30 +0300

    add carsAPI

---
+++ src/api/cardsAPI.ts
@@ -0,0 +1,39 @@
+import {instance} from './instance';
+
+export const cardsAPI = {
+    getCards: (args: GetCardType) => {
+        return instance.get<GetCardsResponseType>('/cards/card', {params: args});
+    },
+};
+
+export type GetCardType = {
+    cardAnswer?: string
+    cardQuestion?: string
+    cardsPack_id: string
+    min?: number
+    max?: number
+    sortCards?: string
+    page?: number
+    pageCount?: number
+}
+export type CardsType = {
+    answer: string
+    question: string
+    cardsPack_id: string
+    grade: number
+    shots: number
+    user_id: string
+    created: Date
+    updated: Date
+    _id: string
+}
+export type GetCardsResponseType = {
+    cards: Array<CardsType>
+    cardsTotalCount: number
+    maxGrade: number
+    minGrade: number
+    page: number
+    pageCount: number
+    packUserId: string
+}
+
