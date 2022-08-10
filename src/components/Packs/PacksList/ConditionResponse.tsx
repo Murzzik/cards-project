f9b5859d9adf3faf156ec512e923c1c455a0e788
1t-n00b <mummintrol@gmail.com>
2022-08-10 18:57:57 +0300

    add Button/condition Packs

---
+++ src/components/Packs/PacksList/ConditionResponse.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Button} from '@material-ui/core';
+import {addNewPack} from '../../../store/reducers/packs-reducer';
+import {useAppDispatch, useAppSelector} from '../../../store/store';
+
+const ConditionResponse: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const myId = useAppSelector(state => state.auth.user._id);
+    const isMyPacks = useAppSelector(state => state.packsParameter.user_id) === myId;
+
+    const newPackHandler = () => {
+        const name = 'New pack success added';
+        dispatch(addNewPack(name));
+    };
+    return (
+        <div>
+            {isMyPacks ?
+                <div>
+                    <div style={{textAlign: 'center', fontSize: '28px', color: 'white'}}>Нет таких паков</div>
+                    <Button variant={'contained'} color={'primary'} onClick={newPackHandler} style={{left: '50%'}}>Add new pack</Button>
+                </div>
+                :
+                <div style={{textAlign: 'center', fontSize: '28px', color: 'white'}}>Нет таких паков</div>
+            }
+        </div>
+    );
+};
+
+export default ConditionResponse;
--- src/components/Packs/PacksList/PacksList.tsx
+++ src/components/Packs/PacksList/PacksList.tsx
@@ -1,25 +1,20 @@
 import * as React from 'react';
-import {useAppDispatch, useAppSelector} from '../../../store/store';
+import {useAppSelector} from '../../../store/store';
 import TablesContainer from './Table/TablesContainer';
-import {Button} from '@material-ui/core';
-import {addNewPack} from '../../../store/reducers/packs-reducer';
+import ConditionResponse from './ConditionResponse';
 
 type PackListPopsType = {}
 
 export const PackList: React.FC<PackListPopsType> = () => {
     const isPacks = useAppSelector(state => state.packs.cardPacks).length > 0;
-    const dispatch = useAppDispatch();
-    const newPackHandler = () => {
-        const name = 'New pack success added';
-        dispatch(addNewPack(name));
-    };
+
 
     return (
         <div>
             {isPacks ?
                 <TablesContainer/>
                 :
-                <Button variant={'contained'} color={'primary'} onClick={newPackHandler} style={{left: '50%'}}>Add new pack</Button>
+              <ConditionResponse/>
             }
         </div>
     );
