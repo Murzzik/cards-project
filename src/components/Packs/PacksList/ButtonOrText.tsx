0b9e1d33badcd4e686818e0f0b551d6f1f9d9b75
1t-n00b <mummintrol@gmail.com>
2022-08-11 13:04:34 +0300

    ref addModall to ButtonOrText.tsx

--- src/components/Packs/PacksList/ButtonOrText.tsx
+++ src/components/Packs/PacksList/ButtonOrText.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import {Button} from '@material-ui/core';
-import {addNewPack} from '../../../store/reducers/packs-reducer';
-import {useAppDispatch, useAppSelector} from '../../../store/store';
+import {useAppSelector} from '../../../store/store';
+import AddNewPackModal from '../../common/universalModal/AddNewPackModal';
 
 const ButtonOrText: React.FC = () => {
-    const dispatch = useAppDispatch();
     const myId = useAppSelector(state => state.auth.user._id);
     const isMyPacks = useAppSelector(state => state.packsParameter.user_id) === myId;
 
-    const newPackHandler = () => {
-        const name = 'New pack success added';
-        dispatch(addNewPack(name));
-    };
     return (
         <div>
             {isMyPacks ?
                 <div>
                     <div style={{textAlign: 'center', fontSize: '28px', color: 'white'}}>Нет таких паков</div>
-                    <Button variant={'contained'} color={'primary'} onClick={newPackHandler} style={{left: '50%'}}>Add new pack</Button>
+                    <div style={{textAlign: 'center'}}>
+                        <AddNewPackModal/>
+                    </div>
+
                 </div>
                 :
                 <div style={{textAlign: 'center', fontSize: '28px', color: 'white'}}>Нет таких паков</div>
