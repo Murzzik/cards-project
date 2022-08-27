50918a4232af472d73b707b8fcde010c4418f7f6
1t-n00b <mummintrol@gmail.com>
2022-08-27 19:05:56 +0300

    add antd Spin

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
+import {Spin} from 'antd';
 import style from './Preloader.module.css';
-import preloader from '../../../assets/images/Double Ring-1s-111px.svg';
 
 const Preloader: React.FC = () => {
     return (
-        <div className={style.preloader}>
-            <img src={preloader} alt="preloader image"/>
-            {/*<CircularProgress />*/}
-        </div>
+        <Spin className={style.preloader} size={'large'}/>
+
     );
 };
 
